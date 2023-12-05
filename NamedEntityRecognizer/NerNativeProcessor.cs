f02063d98af0c69e555a6d61dde01cf19e5412c9
Leftyx <171024+Leftyx@users.noreply.github.com>
2023-12-05 18:12:10 +0000

    updated packages to v 3.0

---
+++ NamedEntityRecognizer/NerNativeProcessor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.TorchSharp;
+
+namespace NamedEntityRecognizer
+{
+    internal class NerNativeProcessor
+    {
+        private class TestSingleSentenceData
+        {
+            public required string Sentence;
+            public required string[] Label;
+        }
+
+        private class Label
+        {
+            public string Key { get; set; }
+        }
+
+        public void TestSimpleNer()
+        {
+            MLContext ML = new();
+
+            var labels = ML.Data.LoadFromEnumerable(
+                new[] {
+                new Label { Key = "PERSON" },
+                new Label { Key = "CITY" },
+                new Label { Key = "COUNTRY"  }
+                });
+
+            var dataView = ML.Data.LoadFromEnumerable(
+                new List<TestSingleSentenceData>(new TestSingleSentenceData[] {
+                    new()
+                    {   // Testing longer than 512 words.
+                        Sentence = "Barbara and Frank live in the Liechtenstein",
+                        Label = new string[]{"PERSON", "0", "PERSON", "0", "0", "0", "COUNTRY"}
+                    },
+                     new()
+                     {
+                        Sentence = "Alice and Bob live in the USA",
+                        Label = new string[]{"PERSON", "0", "PERSON", "0", "0", "0", "COUNTRY"}
+                     },
+                }));
+
+            var chain = new EstimatorChain<ITransformer>();
+
+            var estimator = chain.Append(ML.Transforms.Conversion.MapValueToKey("Label", keyData: labels))
+               .Append(ML.MulticlassClassification.Trainers.NameEntityRecognition(outputColumnName: "outputColumn"))
+               .Append(ML.Transforms.Conversion.MapKeyToValue("outputColumn"));
+
+            // var estimatorSchema = estimator.GetOutputSchema(SchemaShape.Create(dataView.Schema));
+
+            var transformer = estimator.Fit(dataView);
+
+            // var transformerSchema = transformer.GetOutputSchema(dataView.Schema);
+            // var predictionIDV = transformer.Transform(test);
+
+            transformer.Dispose();
+        }
+    }
+}
